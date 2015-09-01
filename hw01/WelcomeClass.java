ca6a1c116983edb30b221aef904eab5febf602df
tjj218 <tjj218@lehigh.edu>
2015-09-01 16:37:48 +0000

    updated WelcomeClass

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -1,7 +1,12 @@
+//Tamara Johnson
+//CSE 002 310
+//HW01
+//9/1/2015
 // define class
 public class WelcomeClass{
     //add main method
     public static void main(String[] args){
+    // make sure two\\ are added to avoid error
         
         //print my statement
         System.out.println("                ----------- ");
@@ -14,7 +19,7 @@ public class WelcomeClass{
         System.out.println("              v  v  v  v  v  v ");
         System.out.println("Hi, I'm Tamara from Orange NJ, I am in the class of 2018");
         System.out.println("I live in the Kappa Alpha Theta house and I'm majoring in");
-        System.out.println("                Industrial Engineering!                 ");
+        System.out.println("                Industrial Engineering!!                 ");
     }
 
     }
