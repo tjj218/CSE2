c245336b3bfca344d4354817a4269dd3f7f694b1
tjj218 <tjj218@lehigh.edu>
2015-09-08 16:07:21 +0000

    updated Arithmetic

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -51,19 +51,13 @@ public class Arithmetic{
         System.out.println(nSocks+ " pairs of Socks were purchased at " + sockCost$ +" per pair");
         System.out.println(nEnvelopes+ " box of Envelopes was purchased at " + envelopeCost$);
         System.out.println(nGlasses+ " drinking glasses were purchased at "+glassCost$+" per glass");
-        System.out.printf("The total cost for the Socks before tax is "+totalSockCost$+ " and the tax is %.2f", sockTax$);
-        //add 'System.out.println();' so that it creates a new line
-        System.out.println();
-        System.out.printf("The total cost for the Envelopes before tax is "+totalEnvelopeCost$+ " and the tax is %.2f", envelopeTax$);
-        System.out.println();
-        System.out.printf("The total cost for the Glasses before tax is "+totalGlassCost$+ " and the tax is %.2f", glassesTax$);
-        System.out.println();
-        System.out.printf("The total purchase cost before tax comes to %.2f", preTaxTotal$);
-        System.out.println();
-        System.out.printf("The total sale tax is %.2f", totalTax$);
-        System.out.println();
-        System.out.printf("The final total cost including tax is %.2f", totalTransCost$);
-        System.out.println();
+        System.out.printf("The total cost for the Socks before tax is "+totalSockCost$+ " and the tax is %.2f %n", sockTax$);
+        //add '%n' so that it creates a new line
+        System.out.printf("The total cost for the Envelopes before tax is "+totalEnvelopeCost$+ " and the tax is %.2f %n", envelopeTax$);
+        System.out.printf("The total cost for the Glasses before tax is "+totalGlassCost$+ " and the tax is %.2f %n", glassesTax$);
+        System.out.printf("The total purchase cost before tax comes to %.2f %n", preTaxTotal$);
+        System.out.printf("The total sale tax is %.2f %n", totalTax$);
+        System.out.printf("The final total cost including tax is %.2f %n", totalTransCost$);
     }
 
         
